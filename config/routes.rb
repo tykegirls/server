a91d5d179c26e4b389de02141c285507b6cfa845
kimill1394 <kimill1394@gmail.com>
2017-10-17 06:05:59 +0900

    update config: develop-database setting and root-page changing to test-page

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
+  resources :rewards
+  resources :savings
+  resources :group_members
+  resources :groups
+  resources :fixed_exps
+  resources :receipts
+  resources :small_cates
+  resources :big_cates
+  resources :cards
+  resources :users
+  # get 'home/index'
+  root :to => 'home#index'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
